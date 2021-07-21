82c70b296bdf42c3e8d0a3f15e8a9c4dc46a6f0d
Rakshit Gupta <2509rakshitgupta@gmail.com>
2021-07-21 16:09:22 +0530

    Add files via upload

---
+++ Mypratice11_Exceptionhandling01.java
@@ -0,0 +1,29 @@
+package com.rakshit;
+import java.util.Scanner;
+
+public class Mypratice11_Exceptionhandling01 {
+    public static void main(String[] args)
+{
+    int a,b,c;
+
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("Eneter 2 numbers");
+    a=sc.nextInt();
+    b=sc.nextInt();
+
+
+    try
+    {
+        c=a/b;
+        System.out.println("Division is" +c);
+    }
+    catch(ArithmeticException e)
+    {
+        System.out.println("Denominator should not be 0, try again");
+    }
+    System.out.println("Bye");
+
+
+}
+}
